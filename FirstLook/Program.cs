2a9cc2601cfff3bca915616b57d1767f15c11718
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2022-08-22 12:09:52 +0200

    Sync

--- FirstLook/Program.cs
+++ FirstLook/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System.Reactive.Linq;
+
+var fruits = new string[]
+{
+    "old-banana",
+    "apple",
+    "dirty-banana",
+    "apple"
+}.ToObservable();
+fruits
+  // Lösung hier...
+  .Where(x => !x.StartsWith("old-"))
+  .Select(x => x.Replace("dirty-", ""))
+  //.Take(2)
+  .Subscribe(item => Console.WriteLine(item));
+
+// out:
+/*
+  apple
+  banana
+ */
+
+//Console.WriteLine("Hello World!".Replace("World!", "Teilnehmer!"));
+Console.ReadLine();
